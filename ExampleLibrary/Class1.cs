4147948687dcb599053c73d77a68521f7ee23621
Jerry Conner <jerry.conner@gmail.com>
2013-06-21 21:56:59 -0400

    Initial checkin

---
+++ ExampleLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleLibrary
+{
+    public class MathClass
+    {
+        public static int Factorial(int num)
+        {
+            if (num < 2) return num;
+            return num * Factorial(num - 1);
+        }
+    }
+}
+
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ExampleLibrary;
+
+namespace Tests
+{
+    [TestClass]
+    public class MathClassTests
+    {
+        [TestMethod]
+        public void TestFactorial()
+        {
+            Assert.AreEqual(2 * 3 * 4 * 5, MathClass.Factorial(5));
+            Assert.AreEqual(2 * 3 * 4 * 5 * 6 * 7, MathClass.Factorial(7));
+        }
+    }
+}
