362e969f195e51cf13e14b8563f5afa917b1c7e4
Jerry Conner <jerry.conner@gmail.com>
2013-06-21 22:26:13 -0400

    Getting the values from a dictionary as a list example

---
+++ ExampleLibrary/DictionaryHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleLibrary
+{
+    public class DictionaryHelper
+    {
+        public static Dictionary<string, int> HowMuchILikeEachThing
+        {
+            get;
+            set;
+        }
+
+        public static IList<int> Values
+        {
+            get { return HowMuchILikeEachThing.Values.ToList(); }
+        }
+
+    }
+}
---
+++ Tests/DictionaryHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ExampleLibrary;
+
+namespace Tests
+{
+    [TestClass]
+    public class DictionaryHelperTests
+    {
+        [TestMethod]
+        public void TestGettingValuesAsList()
+        {
+            // Arrange: Let's say I have a list of things and how much I like them
+            DictionaryHelper.HowMuchILikeEachThing = new Dictionary<string, int>();
+            DictionaryHelper.HowMuchILikeEachThing.Add("Banana", 10);
+            DictionaryHelper.HowMuchILikeEachThing.Add("Peanut Butter", 50);
+
+            // Act: Now say I want the values in that dictionary as a list
+            var list = DictionaryHelper.Values;
+
+            // Assert:
+            Assert.IsNotNull(list, "The list should not be null");
+            Assert.IsInstanceOfType(list, typeof(IList<int>), "The list should be some kind of IList<int>");
+            Assert.AreEqual(10, list[0]);
+            Assert.AreEqual(50, list[1]);
+            Assert.AreEqual(DictionaryHelper.HowMuchILikeEachThing.Count, list.Count);
+        }
+    }
+}
